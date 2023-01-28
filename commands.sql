62c071281d24ebf91901538e985df196ef3a8768
AndrésP <andukial@gmail.com>
2023-01-28 16:01:20 -0600

    primer commit

---
+++ commands.sql
@@ -0,0 +1,21 @@
+
+sudo su postgres
+psql -U postgres
+
+CREATE TABLE tabla1 
+(
+    columna1 varchar(10),
+    columna2 varchar(10),
+    columna3    int
+)
+
+INSERT INTO tabla1 (columna1, columna2, columna3) 
+VALUES (Hola, Adios, 10),
+("Hola", "Adios", 10),
+("Hola", "Adios", 10),
+("Hola", "Adios", 10),
+("Hola", "Adios", 10),
+("Hola", "Adios", 10),
+("Hola", "Adios", 10),
+("Hola", "Adios", 10),
+("Hola", "Adios", 10);
